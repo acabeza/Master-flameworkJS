09e511ddaa38b7539d69dddf56adf7311de65635
acabeza.m.tr@gmail.com <acabeza.m.tr@gmail.com>
2023-05-20 22:56:22 +0200

    Addded component and routing

--- AprendiendoAngular/src/app/app.routing.ts
+++ AprendiendoAngular/src/app/app.routing.ts
@@ -19,6 +19,7 @@ const appRoutes: Routes = [
     {path: 'formulario', component: FormularioComponent},
     {path: 'peliculas', component: PeliculasComponent},
     {path: 'pagina-de-pruebas', component: PaginaComponent},
+    {path: 'pagina-de-pruebas/:nombre/:apellidos', component: PaginaComponent},
     {path: '**', component: ErrorComponent}
 ];
 
--- AprendiendoAngular/src/app/components/pagina/pagina.component.ts
+++ AprendiendoAngular/src/app/components/pagina/pagina.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivatedRoute, Params } from '@angular/router';
 @Component({
   selector: 'app-pagina',
   templateUrl: './pagina.component.html',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaginaComponent implements OnInit {
 
-  constructor() { }
+  public nombre: string = "";
+  public apellidos: string = "";
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router) {
+   }
 
   ngOnInit(): void {
+    this._route.params.subscribe((params: Params) => {
+      this.nombre = params['nombre'];
+      this.apellidos = params['apellidos'];
+    })
+  }
+
+  redireccion() {
+    this._router.navigate(['/pagina-de-pruebas', 'Alejandro', 'Cabeza Muñoz']);
   }
 
 }
